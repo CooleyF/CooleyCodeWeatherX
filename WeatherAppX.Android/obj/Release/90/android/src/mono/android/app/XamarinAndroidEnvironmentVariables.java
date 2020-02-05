70749769b1e73aedca2eed9a93081e8a2ef8e0dc
jecool17 <51241657+jecool17@users.noreply.github.com>
2020-02-05 13:20:10 -0500

    u
    u

---
+++ WeatherAppX.Android/obj/Release/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_DEBUG", "gen-compact-seq-points",
+		"XAMARIN_BUILD_ID", "8c6d794a-f341-4417-a694-6e3b219e77d9",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
